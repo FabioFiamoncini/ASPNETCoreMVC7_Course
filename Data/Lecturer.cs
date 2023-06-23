fc3a47d52e976d0345da46d0f74e89019fc04dc2
Fábio <fiamoncini.f@gmail.com>
2023-06-23 16:58:49 -0300

    commit 2

---
+++ Data/Lecturer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPNETCoreMVC7_Course.Data;
+
+public partial class Lecturer
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+}
